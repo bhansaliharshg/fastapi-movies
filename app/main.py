9df84a4351e6f806c3c8f3558c65df2a2d9117ff
Harsh Bhansali <harshbhansali@Harshs-MacBook-Air.local>
2023-03-24 11:26:59 -0400

    Updated api

--- app/main.py
+++ app/main.py
@@ -31,6 +31,7 @@ class Movie(BaseModel):
 async def root():
     conn = setup()
     cursor = conn.execute('SELECT * FROM Movie')
+    conn.close()
     return [convertToMovie(movie) for movie in cursor.fetchall()]
 
 @app.get('/movie/id/{id}', response_description='Retrieve Movie By Id', response_model=Movie)
@@ -38,6 +39,7 @@ async def getMovieById(id:str):
     conn = setup()
     query = 'SELECT * FROM Movie where id = ' + id
     cursor = conn.execute(query)
+    conn.close()
     return convertToMovie(cursor.fetchone())
 
 @app.get('/movie/name/{name}', response_description='Search Movie By Name', response_model=List[Movie])
@@ -45,18 +47,34 @@ async def getMovieByName(name: str):
     conn = setup()
     query = 'SELECT * FROM Movie where title LIKE \'%' + name + '%\''
     cursor = conn.execute(query)
+    conn.close()
     return [convertToMovie(movie) for movie in cursor.fetchall()]
 
 @app.get('/movie/genre/{genre}', response_description='Search Movie By Genre', response_model=List[Movie])
-async def getMovieByName(genre: str):
+async def getMovieByGenre(genre: str):
     conn = setup()
     query = 'SELECT * FROM Movie where genre LIKE \'%' + genre + '%\''
     cursor = conn.execute(query)
+    conn.close()
     return [convertToMovie(movie) for movie in cursor.fetchall()]
 
-@app.post('/movie/{id}/rent')
-async def rentAMovie(movie: Movie):
-    pass
+@app.get('/movie/{id}/rent')
+async def rentAMovie(id: str):
+    conn = setup()
+    available, movie = checkIfMovieAvailable(conn, id)
+    if available:
+        query = 'UPDATE Movie SET rented = ' + str(movie.rented+1) + ', available = ' + str(movie.available-1) + 'WHERE id = ' + id
+        cursor = conn.execute(query)
+        conn.commit()
+    conn.close()
+
+def checkIfMovieAvailable(conn, id: str):
+    query = 'SELECT * FROM movies WHERE id = ' + id
+    cursor = conn.execute(query)
+    movie = convertToMovie(cursor.fetchone())
+    if movie.available > 0 and movie.rented <= 10:
+        return True, movie
+    return False, movie
 
 def convertToMovie(result):
     return Movie(id=result[0], title=result[1], isAdult=result[2], year=result[3], runtime=result[4], genre=result[5].split(',') if result[5] else [], available=result[6], rented=result[7])
